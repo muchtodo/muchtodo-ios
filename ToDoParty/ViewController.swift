faf6ad078a8aa6f31a93f358720b28029d90e619
Belle Beth Cooper <bellebethcooper@gmail.com>
2018-02-18 10:45:13 +1100

    Initial Commit

---
+++ ToDoParty/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ToDoParty
+//
+//  Created by Belle Beth Cooper on 18/2/18.
+//  Copyright © 2018 Hello Code. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
